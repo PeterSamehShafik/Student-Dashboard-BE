fb0810f945c91c829bf950002f34c4ff7c1bf1c4
PeterSamehShafik <petosameh82@gmail.com>
2024-05-17 23:10:27 +0300

    cors-try

--- modules/index.router.js
+++ modules/index.router.js
@@ -14,7 +14,9 @@ import userModel from '../DB/models/user.model.js'
 export const appRouter = (app) => {
     app.use(express.json({}));
     app.use(express.urlencoded({ extended: true }))
-    app.use(cors({}));
+    app.use(cors({
+        origin: '*'
+    }));
 
     //Returns request endpoint and time taken to execute it
     if (process.env.MODE === 'DEV') {
